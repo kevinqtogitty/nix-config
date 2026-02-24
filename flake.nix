c25fd81e62a77020db628176a1154544f03adab7
Kevin To <kevin.to@gmail.com>
2026-02-23 20:31:57 -0500

    🎨 Add nix-darwin support
    adds cross platform compatability linux/macOS

--- flake.nix
+++ flake.nix
@@ -1,20 +1,19 @@
 {
   description = "Base System";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
-  outputs = { self, nixpkgs, ... }@inputs: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, nix-darwin, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-      ];
+      modules = [ ./configuration.nix ];
+    };
+    darwinConfigurations.my-macbook = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [ ./darwin-configuration.nix ];
     };
   };
 }
