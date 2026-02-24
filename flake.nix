1590d272bc997256fa6225b5d11fa356e861ee9b
Kevin To <kevinq.to@gmail.com>
2026-02-23 19:31:52 -0500

    initial

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Base System";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
